d9b4562ef8a6aa097c0d89ba561c45f18c6a9b32
Brian <bcf.vandenbroek@student.avans.nl>
2014-10-13 13:39:45 +0200

    HotelRoom entiteit

--- DomainModel/Repositories/IHotelRoomRepository.cs
+++ DomainModel/Repositories/IHotelRoomRepository.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DomainModel
+namespace DomainModel.Repositories
 {
-    public class Class1
+    public interface IHotelRoomRepository
     {
+
     }
 }
